37461e3772bfb6baf7cefda7f39562ff45f2f232
María del Pilar Alonso <mariadelpilar.alonso@ingka.ikea.com>
2023-10-09 11:38:10 +0200

    feat :adding card and listcard components

---
+++ src/components/list-card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ListCard extends React.Component {
+    render() {
+      return <div className="github-profile">One github profile</div>;
+    }
+  }
+  
+  
+  
+  export default ListCard;
